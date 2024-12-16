593bca9d63da96464510a9d211549ee1b6c80963
TuanNguyenKim <146519793+PoppyPush@users.noreply.github.com>
2024-12-16 16:03:42 +0700

    Delete model directory

--- model/Secret.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const secretSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    secret: { type: String, required: true }
-});
-
-module.exports = mongoose.model('Secret', secretSchema);
--- model/User.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-    username: { type: String, unique: true, required: true },
-    password: { type: String, required: true } // Lưu mật khẩu đã băm
-});
-
-module.exports = mongoose.model('User', userSchema);
